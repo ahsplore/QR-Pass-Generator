d52b1b63d538156074a752e403ac5d7d849aa557
ahsplore <anannyakundu07@gmail.com>
2025-03-25 18:46:20 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
+import QRpassgenerator from './QRPassGenerator.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <QRpassgenerator />
   </StrictMode>,
 )
